66e01b10660bf5f48afb6347c0cf3fb4a61a3bc4
ezeYaniv <brener.yaniv@gmail.com>
2021-02-05 23:11:04 -0700

    add logic to check for dashed quantities ('1-2 cups' and '1 to 2 cups')

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const jsdom = require("jsdom");
 const ejs = require("ejs");
 //note: I also installed jquery with npm i jquery, it is required in the asynchronous function below.
 
-const createList = require(__dirname+"/createList.js");
+const createList = require(__dirname + "/createList.js");
 
 //installed npm i fraction.js to convert decimals back into fractions
 const Fraction = require("fraction.js");
@@ -31,15 +31,14 @@ let servingsOld;
 let servingsNew;
 let url;
 
-
 app.get("/", function (req, res) {
-  res.render("home", { oldList: oldList, newList: newList, servingsOld: servingsOld, servingsNew: servingsNew, url: url } );
+  res.render("home", { oldList: oldList, newList: newList, servingsOld: servingsOld, servingsNew: servingsNew, url: url });
 });
 
 //this captures the user's HTML url & serving # form submission
 app.post("/", function (req, res) {
   url = req.body.url;
-  if(url.startsWith("http") == false) {
+  if (url.startsWith("http") == false) {
     url = "http://" + url;
   }
   servingsOld = req.body.servingsOld;
@@ -56,38 +55,63 @@ app.post("/", function (req, res) {
     const $ = (jQuery = require("jquery")(dom.window));
 
     $(dom.window.document).ready(function () {
-      
       let listItems;
 
       /************************* CHECKS WPRM SITES VS. NON-WPRM ******************************** */
-      if($(".wprm-recipe-ingredients").length > 0) {
+      if ($(".wprm-recipe-ingredients").length > 0) {
         listItems = $(".wprm-recipe-ingredients > li.wprm-recipe-ingredient").get();
       } else {
-      
-        listItems = $(":header").filter(function() { 
-          return $(this).text() == "Ingredients"
-          }).siblings("ul").children("li").get();  //this code selects li children of ul siblings of any header containing "Ingredients"
-        }
+        listItems = $(":header")
+          .filter(function () {
+            return $(this).text() == "Ingredients";
+          })
+          .siblings("ul")
+          .children("li")
+          .get(); //this code selects li children of ul siblings of any header containing "Ingredients"
+      }
 
-        oldList = [];
-        newList = [];
-        
+      oldList = [];
+      newList = [];
+      let liNumber;
+      let newNum;
+
       //this code selects each li in the array, parses the numbers, converts to new quantity
       for (let i = 0; i < listItems.length; i++) {
-
         //grab text content of each listItems array
         //NOTE: using trim because otherwise it adds new line break \n characters and breaks the regex
         let currentLi = listItems[i].textContent.trim();
 
-        // use this regex to extract numbers from LI text
+        // regex expressions to extract numbers from list items
+        const regexDash = /\d+(-|–)\d+\s*\D/;
+        const regexTo = /\d+\s*to\s*\d+/;
         const regexAll = /(\d*)(\.\d+|(\s+\d*\s*)?\s*\/\s*\d+|(\-\d*\s*)?\s*\/\s*\d+|(\&\d*\s*)?\s*\/\s*\d+|\s*[\u00BC-\u00BE\u2150-\u215E]+|(\-[\u00BC-\u00BE\u2150-\u215E])?)?/;
-        let liNumber = regexAll.exec(currentLi);
 
-        //use the numeric-quantity npm library to convert fraction/number strings to numbers
-        let num = numericQuantity(liNumber[0]);
-        let newNum = num * servingsMult;
+        //This code checks for "1-2" cups
+        if (regexDash.exec(currentLi)) {
+          liNumber = regexDash.exec(currentLi);
+          liNumber[0] = liNumber[0].replace(/\s+/, "").slice(0, -1); //trims spaces and the first non-digit off the end
+          let newNumArray = liNumber[0].replace(/(-|–)/, "-").split("-"); //creates new array with 2 numbers split at the "-"
+          for (let i = 0; i < newNumArray.length; i++) {
+            newNumArray[i] *= servingsMult;
+          }
+          newNum = newNumArray[0] + "-" + newNumArray[1];
+        } else if (regexTo.exec(currentLi)) {
+          liNumber = regexTo.exec(currentLi);
+          // liNumber[0] = liNumber[0].replace(/\s+/, "").slice(0, -1); //removes spaces
+          let newNumArray = liNumber[0].replace(/\s/g, "").split("to"); //creates new array with 2 numbers split at the "to"
+          for (let i = 0; i < newNumArray.length; i++) {
+            newNumArray[i] *= servingsMult;
+          }
+          newNum = newNumArray[0] + " to " + newNumArray[1];
+        } else {
+          // use this regex to extract numbers from LI text
+          liNumber = regexAll.exec(currentLi);
 
-        /*//code to create mixed fraction NOTE: Fraction.js library can do this below, with toFraction(true)
+          //use the numeric-quantity npm library to convert fraction/number strings to numbers
+          let num = numericQuantity(liNumber[0]);
+          newNum = num * servingsMult;
+
+          /*//code to create mixed fraction NOTE: Fraction.js library can do this below, with toFraction(true)
                 // if newNum>1, split newNum at the '.', then 
                 if(newNum>1) {
                     let numDen = newNum.toString().split('.');
@@ -97,44 +121,35 @@ app.post("/", function (req, res) {
                 console.log(a[1]);
                 };*/
 
-        //convert newNum from decimal to mixed fraction string (if newNum is .333, .666 or .999, change back to a string fraction so that the toFraction function doesn't give things like 500/133)
-        if (isNaN(newNum)) {
-          newNum = "";
-        } else if (newNum == 0.333) {
-          newNum = "1/3";
-        } else if (newNum == 0.666) {
-          newNum = "2/3";
-        } else if (newNum == 0.999) {
-          newNum = "1";
-        } else {
-          newNum = new Fraction(newNum).toFraction(true);
+          //convert newNum from decimal to mixed fraction string (if newNum is .333, .666 or .999, change back to a string fraction so that the toFraction function doesn't give things like 500/133)
+          if (isNaN(newNum)) {
+            newNum = "";
+          } else if (newNum == 0.333) {
+            newNum = "1/3";
+          } else if (newNum == 0.666) {
+            newNum = "2/3";
+          } else if (newNum == 0.999) {
+            newNum = "1";
+          } else {
+            newNum = new Fraction(newNum).toFraction(true);
+          }
         }
 
         //replace old quantities with the new for an HTML list
         let newLi = currentLi.replace(liNumber[0], newNum);
+
+        //add original and new list items to arrays
         oldList.push(currentLi);
         newList.push(newLi);
-
-        /*//find all vulgar fractions and replace them
-                let regexVF = /[\u00BC-\u00BE\u2150-\u215E]/;
-                let liVF = regexVF.exec(currentLi);
-                if(liVF) {
-                let liNum = numericQuantity(liExec[0]);
-                console.log(liNum);
-                }*/
       }
-      
+
       res.redirect("/");
-      
     });
-    
   })();
 });
 
-
 app.get("/results", function (req, res) {
   res.render("results", { oldList: oldList, newList: newList, servingsOld: servingsOld, servingsNew: servingsNew });
-
 });
 
 let port = process.env.PORT;
